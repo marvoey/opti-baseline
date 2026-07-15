20ce4ab66deba3d07a46d02bdf349ad044c18042
Marvin Oey <marvoey99@gmail.com>
2026-07-15 11:10:19 -0400

    Add something to help with SSE timeouts

--- app/api/opal/response/route.ts
+++ app/api/opal/response/route.ts
@@ -70,6 +70,7 @@ export function GET(request: NextRequest) {
       'Content-Type': 'text/event-stream',
       'Cache-Control': 'no-cache',
       'Connection': 'keep-alive',
+      'X-Accel-Buffering': 'no',
     },
   });
 }
